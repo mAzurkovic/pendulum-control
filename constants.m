e13c86624a1d5e294f24a5f55aa64bd5a8b10fbb
Mattias Zurkovic <mattias@zurkovic.com>
2021-01-15 10:44:37 -0800

    init

---
+++ constants.m
@@ -0,0 +1,13 @@
+RPM_CONV = 0.10472; 
+
+% Electrical Impedance %
+Ze_num = [1];
+Ze_den = [.002 1.0];
+
+% Mechanical Impedance %
+Zm_num = [1];
+Zm_den = [0.00013 0.0000102];
+
+% Constants %
+K_m = 0.062;
+K_b = 16.11;
