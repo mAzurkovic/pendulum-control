0cb69856a0dc6515ae4f4402e005e29eddd25a38
Mattias Zurkovic <mattias@zurkovic.com>
2021-01-16 18:13:45 -0800

    modify constants

--- constants.m
+++ constants.m
@@ -1,13 +1,16 @@
+% RE 30 Graphite Brushless
+
 RPM_CONV = 0.10472; 
 
 % Electrical Impedance %
 Ze_num = [1];
-Ze_den = [.002 1.0];
+Ze_den = [.034 0.196];
 
 % Mechanical Impedance %
 Zm_num = [1];
-Zm_den = [0.00013 0.0000102];
+Zm_den = [0.00337 0.000805];
+%00013
 
 % Constants %
-K_m = 0.062;
-K_b = 16.11;
+K_m = 0.0139;
+K_b = 71.7332;
