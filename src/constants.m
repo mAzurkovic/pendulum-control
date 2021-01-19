8f39377a485119c035a2d3e262d787ee1ff90ff1
Mattias Zurkovic <mattias@zurkovic.com>
2021-01-18 17:45:00 -0800

    add solidworks models

--- src/constants.m
+++ src/constants.m
@@ -4,13 +4,13 @@ RPM_CONV = 0.10472;
 
 % Electrical Impedance %
 Ze_num = [1];
-Ze_den = [.034 0.196];
+Ze_den = [7.46e-7 7.25];
 
 % Mechanical Impedance %
 Zm_num = [1];
-Zm_den = [0.00337 0.000805];
+Zm_den = [(3.9774e-6 + 8.82e-7) 0.000805];
 %00013
 
 % Constants %
-K_m = 0.0139;
-K_b = 71.7332;
+K_m = 0.0452;
+K_b = 22.09592;
